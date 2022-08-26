5795fe0ac07cea6189d6bfee1828d53e13595d70
kittichon phitakkiri <kittichon.dev@gmail.com>
2022-08-26 20:05:46 +0700

    demo 1 of memories app

---
+++ routes/posts.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+} from "../controllers/posts.js";
+import auth from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/", getPosts);
+router.post("/", auth, createPost);
+// patch request is use for updating
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
+
+export default router;
