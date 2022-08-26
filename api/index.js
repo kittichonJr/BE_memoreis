4b72935e2b4364b2fcf3934b008bcbceb132f294
kittichon phitakkiri <kittichon.dev@gmail.com>
2022-08-26 21:00:44 +0700

    add mongoose

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-
+import mongoose from "mongoose";
 import postRoutes from "../routes/posts.js";
 import userRoutes from "../routes/users.js";
 import dotenv from "dotenv";
